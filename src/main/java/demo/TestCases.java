7dd16b3d0e356e02d07628afdd3c93c79c66e1b8
Manis VM <manis.mahadevan@revature.com>
2024-08-14 14:15:34 +0530

    Search function code

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -1,12 +1,18 @@
 package demo;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 
 public class TestCases {
-    ChromeDriver driver;
+    WebDriver driver;
     public TestCases()
     {
         System.out.println("Constructor: TestCases");
@@ -23,12 +29,75 @@ public class TestCases {
 
     }
 
+    public boolean findElement(WebDriver driver, WebElement ele) {
+		boolean flag = false;
+		try {
+			ele.isDisplayed();
+			flag = true;
+		} catch (Exception e) {
+			// System.out.println("Location not found: "+locatorName);
+			flag = false;
+		} finally {
+			if (flag) {
+				System.out.println("Successfully Found element at");
+
+			} else {
+				System.out.println("Unable to locate element at");
+			}
+		}
+		return flag;
+	}
+
+
+    public boolean type(WebElement ele, String text) {
+		boolean flag = false;
+		try {
+			flag = ele.isDisplayed();
+			ele.clear();
+			ele.sendKeys(text);
+			// logger.info("Entered text :"+text);
+			flag = true;
+		} catch (Exception e) {
+			System.out.println("Location Not found");
+			flag = false;
+		} finally {
+			if (flag) {
+				System.out.println("Successfully entered value");
+			} else {
+				System.out.println("Unable to enter value");
+			}
+
+		}
+		return flag;
+	}
+
+
     
-    public  void testCase01(){
+    public  void testCase01() throws InterruptedException{
         System.out.println("Start Test case: testCase01");
         driver.get("https://www.google.com");
-        System.out.println("end Test case: testCase02");
+		// navigating to Chrome using get method
+        WebElement searchBar= driver.findElement(By.xpath("//textarea[@title='Search']"));
+		// getting the xpath for serch bar and verifying the locator using wrapper method
+		this.findElement(this.driver, searchBar);
+		// entering the selenium text i search bar and pressing enter to search
+        this.type(searchBar, "Selenium");
+        searchBar.sendKeys(Keys.ENTER);
+        Thread.sleep(3000); 
+		// Getting the results in list of webelement and iterting it for required value with equals method and verifying it 
+        List <WebElement> SearchResult =  driver.findElements(By.tagName("h3"));
+        for (WebElement element : SearchResult) {
+            if(element.getText().equals("Selenium")){
+                System.out.println("The result page shows the selenium result successfully");
+                break;
+            }else{
+                System.out.println("Unsuccessfull search result found");
+            }  
+        } 
+        
     }
+
+
 
 
 }
